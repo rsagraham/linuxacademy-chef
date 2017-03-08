1755513ceea1e4c55b0128810dfbb9dca2977efc
root <root@rgraham1.mylabserver.com>
2017-03-08 10:58:47 +0000

    Added Environments

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -2,6 +2,8 @@ default["apache"]["sites"]["rgraham2"] = { "site_title" => "rgraham2s website is
 default["apache"]["sites"]["rgraham2b"] = { "site_title" => "rgraham2bs website is coming soon!", "port" => 80, "domain" => "rgraham2b.mylabserver.com" }
 default["apache"]["sites"]["rgraham3"] = { "site_title" => "rgraham3s website is coming soon!", "port" => 80, "domain" => "rgraham3.mylabserver.com" }
 
+default["author"]["name"] = "bob"
+
 case node["platform"]
 when "centos"
 	default["apache"]["package"] = "httpd"
